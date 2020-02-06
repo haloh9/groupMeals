a251f1d76a5ab4fc3340e0715b6fb7760de91af7
Hamza El Meknassi <elmeknassi.h@gmail.com>
2020-02-06 11:26:38 +0100

    Added basic dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id', component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
